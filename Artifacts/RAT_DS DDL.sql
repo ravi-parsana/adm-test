956428caeb566887e98a758844f78267b956fc3b
Ravi Parsana <ravi.parsana@appian-ctr.com>
2019-11-07 11:18:00 -0500

    Message2

---
+++ Artifacts/RAT_DS DDL.sql
@@ -0,0 +1,19 @@
+/*
+Data Store: RAT_DS
+Created: 2019-11-07T15:50:53.460Z
+By: ctr.one.s
+Appian Version: 20.1.0.50
+Target Database: Microsoft SQL Server 14.00.3223
+Database Driver: Microsoft JDBC Driver 7.0 for SQL Server 7.0.0.0
+*/
+
+/* UPDATE DDL */
+/* DROP AND CREATE DDL */
+/* WARNING: The DDL commented out below will drop and re-create all tables.
+    drop table [ratcdt];
+
+    create table [ratcdt] (
+        [id] int not null,
+        primary key ([id])
+    );
+*/
